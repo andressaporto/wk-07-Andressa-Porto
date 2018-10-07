dde7452e03228fd3a17f5658dfcb5f71ac6cf164
andressaporto <andressaporto@users.github.com>
2018-10-07 15:48:51 -0400

    Upload 1
    The Simpsons Website

---
+++ js/bucket.js
@@ -0,0 +1,16 @@
+$( function() {
+    $( "#tabs" ).tabs();
+  } );
+
+
+  $( function() {
+     $( "#draggable" ).draggable();
+     $( "#droppable" ).droppable({
+       drop: function( event, ui ) {
+         $( this )
+           .addClass( "ui-state-highlight" )
+           .find( "p" )
+             .html( "Dropped!" );
+       }
+     });
+   } );
